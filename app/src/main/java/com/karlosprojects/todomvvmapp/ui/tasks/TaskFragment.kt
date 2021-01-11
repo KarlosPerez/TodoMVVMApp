67d5e92cccf916ee1f819c40841c27e161dd6325
Carlos Andres <legomarth@hotmail.com>
2021-01-10 23:32:01 -0500

    feat: create navigation graph and add tasks fragment to navhost

---
+++ app/src/main/java/com/karlosprojects/todomvvmapp/ui/tasks/TaskFragment.kt
@@ -0,0 +1,7 @@
+package com.karlosprojects.todomvvmapp.ui.tasks
+
+import androidx.fragment.app.Fragment
+import com.karlosprojects.todomvvmapp.R
+
+class TaskFragment : Fragment(R.layout.fragment_tasks) {
+}
